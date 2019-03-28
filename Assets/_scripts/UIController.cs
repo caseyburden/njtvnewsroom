eb2bc4df16163b3d63f05aa445c9d9823ac30bed
burdenNJTV <burdenc@njtvnews.org>
2019-03-28 18:37:04 -0400

    newsroom walkthrough

---
+++ Assets/_scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject miniMap;
+
+    private bool showMiniMap = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            showMiniMap = !showMiniMap;
+        }
+
+        miniMap.SetActive(showMiniMap);
+    }
+}
