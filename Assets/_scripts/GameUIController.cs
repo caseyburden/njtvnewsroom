0597e25befce11b96eb1b543475a9d9f01383bfe
Casey Burden <caseyburden@me.com>
2019-03-30 18:42:00 -0400

    added menus

--- Assets/_scripts/GameUIController.cs
+++ Assets/_scripts/GameUIController.cs
@@ -1,22 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameUIController : MonoBehaviour
 {
     public GameObject miniMap;
+    public GameObject gameMenu;
 
-    private bool showMiniMap = false;
+    private FirstPersonController fpsCtrl;
+
+    private bool showMiniMap = true;
+    private bool showGameMenu = false;
+
+    private bool gameIsPaused = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fpsCtrl = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
+       
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameIsPaused = !gameIsPaused;
+        }
+
+
+        if (gameIsPaused)
+        {
+            Paused();
+        }
+        else
+        {
+            Playing();
+        }
+
+    }
+
+    void Paused ()
+    {
+        gameMenu.SetActive(true);
+        miniMap.SetActive(false);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Playing()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
@@ -24,6 +61,15 @@ public class GameUIController : MonoBehaviour
         }
 
         miniMap.SetActive(showMiniMap);
+        gameMenu.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 
 
